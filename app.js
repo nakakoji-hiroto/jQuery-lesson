4062270b7e01318043a65cdbde649e548829f784
nakakoji-hiroto <140766730+nakakoji-hiroto@users.noreply.github.com>
2023-12-14 11:19:17 +0000

    javaScript_lesson 12.14

--- app.js
+++ app.js
@@ -1,10 +1,36 @@
-$(document).ready(function() {
-  $('.box1').slideDown(1000, function (){
-    $('.box1').css({
-      'width': '200px',
-      'height': '100px',
-      'background-color': '#0000FF'
-    }).slideUp(1000);
+// $(document).ready(function() {
+//   $('.box1').slideDown(1000, function (){
+//     $('.box1').css({
+//       'width': '200px',
+//       'height': '100px',
+//       'background-color': '#0000FF'
+//     }).slideUp(1000);
+//   });
+// });
+  
+// $(function(){
+//   $('.box1').mouseover(function(){
+//     $('.box1').css({'background-color': '#0000FF'});
+//   });
+//   $('.box1').mouseout(function(){
+//     $('.box1').css({'background-color': '#FF0000'});
+//   });
+// });
+
+// $(function(){
+//   $('.box1').mouseover(function(){
+//     $('.box1').addClass('box1-ext');
+//   });
+//   $('.box1').mouseout(function(){
+//     $('.box1').removeClass('box1-ext');
+//   });
+// });
+
+$(function(){
+  $('.box1').on('click', function(){
+    $('.box1').addClass('box1-ext');
+  });
+  $('.box1').mouseout(function(){
+    $('.box1').removeClass('box1-ext');
   });
 });
-  
